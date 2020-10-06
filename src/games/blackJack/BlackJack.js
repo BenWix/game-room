a1d350f14a5f427585ee37ecdefb46f6d5e758f3
Nashmeyah <nashalrekabi@gmail.com>
2020-10-05 23:00:20 -0600

    AddCard,ClearHand btns done. next incorporate deck

--- src/games/blackJack/BlackJack.js
+++ src/games/blackJack/BlackJack.js
@@ -1,64 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import PlayerHand from './components/PlayerHand'
-import PlayerActions from './components/PlayerActions'
-import PlayerScore from './components/PlayerScore'
+import PlayerHand from "./components/PlayerHand";
+import PlayerActions from "./components/PlayerActions";
+import PlayerScore from "./components/PlayerScore";
 
+import GameInfo from "../GameInfo";
 
-import GameInfo from '../GameInfo'
-
-//for buttons and alerts and other similar things, please use reactstrap, unless you 
+//for buttons and alerts and other similar things, please use reactstrap, unless you
 //have a specific style you are going for.  https://reactstrap.github.io/
 
 //There is a css file provided in this directory, you can import it here and style anything you like
 //but please do not modify the App.css stylesheet
 
-
 export default class BlackJack extends Component {
-    constructor(props) {
-        super()
-        this.state = {
-            hand: [],
-            score: 0,
-            deck: []
-        }
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      hand: [],
+      score: 0,
+      deck: [],
+    };
+  }
 
-    componentDidMount(){
-        this.setState({
-            deck: getDeck()
-        })
-    }
+  componentDidMount() {
+    this.setState({
+      deck: getDeck(),
+    });
+  }
 
-    render() {
-        // Modify the two following variables to make sure the correct info is presented about the game
-        const about = 'A simple low stakes game of blackjack'
-        const contributors = 'A string of the contributors for this page'
+  render() {
+    // Modify the two following variables to make sure the correct info is presented about the game
+    const about = "A simple low stakes game of blackjack";
+    const contributors = "A string of the contributors for this page";
 
-        
-        return (
-            <div>
-                {/* Player's hand */}
-                <PlayerHand hand={this.state.hand}/>
+    return (
+      <div>
+        {/* Player's hand */}
+        <PlayerHand hand={this.state.hand} />
 
-                <PlayerActions hand={this.state.hand}/>
-                <PlayerScore />
-                {/* GameInfo must remain at the bottom of your root component */}
-                <GameInfo about={about} contributors={contributors} />
-            </div>
-        )
-    }
-
+        <PlayerActions hand={this.state.hand} />
+        <PlayerScore />
+        {/* GameInfo must remain at the bottom of your root component */}
+        <GameInfo about={about} contributors={contributors} />
+      </div>
+    );
+  }
 }
-        function getDeck(count=1) {
-            const deck = []
-            const suits = ['S', 'D', 'C', 'H']
-            const numbers = ['2', '3', '4', '5', '6','7','8','9','10','J','Q','K','A']
-            numbers.forEach(num => {
-                suits.forEach(suit => {
-                    deck.push(num + suit)
-                })
-            });
-            console.log(deck)
-            return deck
-        } 
+function getDeck(count = 1) {
+  const deck = [];
+  const suits = ["S", "D", "C", "H"];
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  numbers.forEach((num) => {
+    suits.forEach((suit) => {
+      deck.push(num + suit);
+    });
+  });
+  console.log(deck);
+  return deck;
+}
--- src/games/blackJack/components/PlayerActions.js
+++ src/games/blackJack/components/PlayerActions.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react'
+import React, { useState } from "react";
 
-export default class PlayerActions extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                
-                {/* A button to Draw a card */}
-                <button>Draw card</button>
+const PlayerActions = () => {
+  const [hand, setHand] = useState([]);
 
-                {/* A button to reset the hand */}
-                <button>Reset Hand</button>
-                
-            </React.Fragment>
-        )
-    }
-}
+  const addCard = () => {
+    setHand([
+      ...hand,
+      {
+        id: hand.length,
+        value: Math.floor(Math.random() * 10) + 1,
+      },
+    ]);
+  };
+
+  const clearHand = () => {
+    setHand([]);
+  };
+
+  return (
+    <React.Fragment>
+      {/* A button to Draw a card */}
+      <ul>
+        {hand.map((card) => (
+          <li key={card.id}>{card.value}</li>
+        ))}
+      </ul>
+
+      <button onClick={addCard}>Draw card</button>
+      {/* should draw one card each time buton is pressed.  */}
+
+      {console.log(hand)}
+
+      {/* A button to reset the hand */}
+      <button onClick={clearHand}>Reset Hand</button>
+
+      {/* resets the hand count i.e resets the state in BlackJack component */}
+    </React.Fragment>
+  );
+};
+export default PlayerActions;
